208aaf22098fceced0353078a31cd9639555228c
St3ve109 <80845787+St3ve109@users.noreply.github.com>
2025-06-11 08:53:40 -0400

    Add files via upload

---
+++ WInget_intune.ps1
@@ -0,0 +1,52 @@
+clear-host
+write-host " __          ___                  _     _    _           _       _            "
+write-host " \ \        / (_)                | |   | |  | |         | |     | |           "
+write-host "  \ \  /\  / / _ _ __   __ _  ___| |_  | |  | |_ __   __| | __ _| |_ ___ _ __ "
+write-host "   \ \/  \/ / | | '_ \ / _` |/ _ \ __| | |  | | '_ \ / _` |/ _` | __/ _ \ '__|"
+write-host "    \  /\  /  | | | | | (_| |  __/ |_  | |__| | |_) | (_| | (_| | ||  __/ |   "
+write-host "     \/  \/   |_|_| |_|\__, |\___|\__|  \____/| .__/ \__,_|\__,_|\__\___|_|   "
+write-host "                        __/ |                 | |                             "
+write-host "                       |___/                  |_|                             "
+write-host ""
+write-host "----------------------------------------"
+Install-Module ps2exe
+write-host "Welcome to the App Maker Script!"
+$fileName = Get-ChildItem -Path . -Filter "IntuneWinAppUtil.exe"
+if ($fileName -match "IntuneWinAppUtil.exe") {
+    write-host "IntuneWinAppUtil is already installed."
+} else {
+    write-host "IntuneWinAppUtil is not installed. Downloading it now..."
+    Invoke-WebRequest -Uri "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/refs/heads/master/IntuneWinAppUtil.exe" -OutFile "IntuneWinAppUtil.exe"
+    write-host "IntuneWinAppUtil.exe has been downloaded successfully."
+}
+$AppName = Read-Host "Enter the name of the app you want to create: "
+winget search $AppName | Select-Object -First 10 | Format-Table -AutoSize
+write-host ""
+if ($LASTEXITCODE -ne 0) {
+    write-host "App '$AppName' not found in winget repository. Please check the name and try again."
+}
+Write-Host "Make sure the app you have chosen is shown and the id is correct, if so press enter to continue otherwise type the correct id of the app"
+$AppId = Read-Host "Enter the correct id of the app (or press Enter to use the name): "
+if ($AppId -eq "") {
+}else {
+    $AppName = $AppId
+}
+write-host "Creating app '$AppName' with id '$AppId'..."
+$AppPath = Read-Host "Enter the path where you want to create the app (default is current directory): "
+if (-not $AppPath) {
+    $AppPath = Get-Location
+}
+New-Item -Path $AppPath -Name "$AppName.ps1" -ItemType File -Force | Out-Null
+set-Content -Path "$AppPath\$AppName.ps1" -Value "winget install $AppName --silent --disable-interactivity --accept-package-agreements" 
+write-host "App '$AppName' created at path: $AppPath\$AppName.ps1"
+write-host "----------------------------------------"
+ps2exe -inputFile "$AppPath\$AppName.ps1" -outputFile "$AppPath\$AppName.exe" -noConsole -requireAdmin -noOutput
+remove-item "$AppPath\$AppName.ps1" -Force
+write-host "Executable '$AppName.exe' created at path: $AppPath\$AppName.exe"
+write-host "----------------------------------------"
+write-host "Now wrap the executable in a .intunewin file using IntuneWinAppUtil.exe"
+.\IntuneWinAppUtil.exe -c $AppPath -s "$AppName.exe" -o $AppPath -q
+write-host "The .intunewin file has been created successfully."
+write-host "----------------------------------------"
+Read-Host "Press Enter to exit the script."
+break
